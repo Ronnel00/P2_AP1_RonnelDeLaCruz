e788287963517feffa7e84c4bba9321020d51f48
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2025-10-27 20:47:13 -0400

    Creando el Contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,6 +1,10 @@
-﻿namespace P2_AP1_RonnelDeLaCruz.DAL
+﻿using Microsoft.EntityFrameworkCore;
+using P2_AP1_RonnelDeLaCruz.Models;
+
+namespace P2_AP1_RonnelDeLaCruz.DAL;
+
+public class Contexto : DbContext
 {
-    public class Contexto
-    {
-    }
+    public DbSet<Registros> Registros { get; set; }
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 }
