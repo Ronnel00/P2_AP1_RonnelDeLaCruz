54d288c01f388bfc61e7040d706323e290adfbb5
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2025-10-27 20:47:47 -0400

    Realizando Migracion

---
+++ Migrations/20251028003908_Iniciando2.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace P2_AP1_RonnelDeLaCruz.Migrations
+{
+    /// <inheritdoc />
+    public partial class Iniciando2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Registros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registros", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Registros");
+        }
+    }
+}
