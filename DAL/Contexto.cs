3909aa0392805acfcb5ff8ad5903169cf09636f3
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2025-11-03 18:01:32 -0500

    Modificando el Contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -7,4 +7,37 @@ public class Contexto : DbContext
 {
     public DbSet<Registros> Registros { get; set; }
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Componente>(entity =>
+        {
+            entity.HasData(
+                new Componente
+                {
+                    ComponenteId = 1,
+                    Descripcion = "Memoria 4GB",
+                    Precio = 1580,
+                    Existencia = 1
+                },
+                new Componente
+                {
+                    ComponenteId = 2,
+                    Descripcion = "Disco SSD 120MB",
+                    Precio = 4200,
+                    Existencia = 8
+                },
+                new Componente
+                {
+                    ComponenteId = 3,
+                    Descripcion = "Tarjeta de Video",
+                    Precio = 10000,
+                    Existencia = 4
+                }
+            );
+        });
+    }
+
 }
