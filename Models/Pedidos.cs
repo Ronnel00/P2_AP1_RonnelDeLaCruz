d3c822f2b365ca8c78e60d78b9dcc0d3ce3f6a25
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2025-11-03 20:16:13 -0500

    Agregando el modelo Pedidos

---
+++ Models/Pedidos.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P2_AP1_RonnelDeLaCruz.Models;
+
+public class Pedidos
+{
+    [Key]
+    public int PedidoId { get; set; }
+
+    [Required]
+    public DateTime Fecha { get; set; } = DateTime.Today;
+
+    [Required, StringLength(100)]
+    public string NombreCliente { get; set; } = string.Empty;
+
+    [Range(0, double.MaxValue)]
+    public decimal Total { get; set; }
+
+    [InverseProperty("Pedido")]
+    public virtual ICollection<PedidosDetalle> Detalles { get; set; } = new List<PedidosDetalle>();
+}
